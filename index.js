09a395c159bd2a1e359c48bcd737ec6ac9ba3b34
Muhammad-Patel <muhpatel2003@gmail.com>
2022-12-25 21:55:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const getColor = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215)
+    const randomColor = '#' + randomNumber.toString(16)
+    document.body.style.backgroundColor = randomColor
+    document.getElementById('cc').innerHTML = randomColor
+    navigator.clipboard.writeText(randomColor)
+}
+getColor()
+
+document.getElementById('btn').addEventListener('click',getColor)
